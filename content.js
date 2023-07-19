638cdad5f04c6ef70d5ca50ec95e352cd12dc0cd
Rustin Dodd <rustin.dodd@ibm.com>
2023-07-19 15:18:36 -0500

    added artist header

--- content.js
+++ content.js
@@ -1,6 +1,12 @@
 const siteContent = {
   'ezrafoster.com': {
-    bandName: 'Ezra Foster'
+    imgPath: '/img/ezra-foster',
+    pageTitle: 'Ezra Foster - Austin, Texas based singer-songwriter.',
+    bandName: 'Ezra Foster',
+    bandBio: 'Ezra Foster is an Austin, Texas based singer-songwriter and multi-instrumentalist whose music exists in a space that blends the various musical traditions of folk, country, and Americana.  Foster’s authentic storytelling is front and center with his unassuming arrangements, minimal production, and unique vocal style.',
+    links: {
+      spotify: 'https://open.spotify.com/artist/1XyfBlg4Ouumub7hivznTf'
+    }
   }
 }
 
