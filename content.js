d173e02fc92caefbb30b1ac2b922f557500857b1
Rustin Dodd <rustin.dodd@ibm.com>
2023-07-25 14:31:08 -0500

    added content for dr floydbeck and crouch and foster

--- content.js
+++ content.js
@@ -3,12 +3,12 @@ const siteContent = {
     imgPath: '/img/ezra-foster',
     pageTitle: 'Ezra Foster - Austin, Texas based singer-songwriter.',
     bandName: 'Ezra Foster',
-    bandBio: 'Ezra Foster is an Austin, Texas based singer-songwriter and multi-instrumentalist whose music exists in a space that blends the various musical traditions of folk, country, and Americana.  Foster’s authentic storytelling is front and center with his unassuming arrangements, minimal production, and unique vocal style.',
+    bandBio: "Ezra Foster is an Austin, Texas based singer-songwriter and multi-instrumentalist whose music exists in a space that blends the various musical traditions of folk, country, and Americana.  Foster's authentic storytelling is front and center with his unassuming arrangements, minimal production, and unique vocal style.",
     links: {
       apple:'https://music.apple.com/us/artist/ezra-foster/999941415',
       spotify: 'https://open.spotify.com/artist/1XyfBlg4Ouumub7hivznTf',
-      amazon: 'https://www.amazon.com/s?k=Ezra+Foster&i=digital-music&search-type=ss&ref=ntt_srch_drd_B00YI4H5YS',
-      pandora: 'https://pandora.app.link/6HKuFPdWF6',
+      amazon: 'https://www.amazon.com/music/player/artists/B00YI4H5YS/ezra-foster',
+      pandora: 'https://www.pandora.com/artist/ezra-foster/ARmkVqn293n6Vrq',
     },
     albums: [
       {
@@ -47,6 +47,64 @@ const siteContent = {
         img: 'fast-cars-and-carry-ons.jpg',
         embed: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/6nRmwbYuENBayNoG16DbQd?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
       },
+    ]
+  },
+  'drfloydbeck.com': {
+    imgPath: '/img/dr-floydbeck',
+    pageTitle: 'Dr. Floydbeck - Austin, Texas based genere-bending musical artist.',
+    bandName: 'Dr. Floydbeck',
+    bandBio: "Dr. Floydbeck is a psychedelic trip into a cross-genre world of beats, atmosphere and melody.  Born in the heart of the pandemic, Dr. Floybeck's debut self-titled EP is best described as lo-fi trip-hop, but hinted at the broader atmosheric cross-genre style that Dr. Floydbeck would adopt.  His latest release, Frontier, is a nearly gapless classic rock inspired space opera.",
+    links: {
+      apple:'https://music.apple.com/us/artist/dr-floydbeck/1541269576',
+      spotify: 'https://open.spotify.com/artist/3EwaiOTx2avXFACye9kLFb',
+      amazon: 'https://www.amazon.com/music/player/artists/B08NZDS2BT/dr-floydbeck',
+      pandora: 'https://www.pandora.com/artist/dr-floydbeck/ARP7hqZ6cpn4q3g',
+    },
+    albums: [
+      {
+        name: 'Frontier',
+        year: '2022',
+        img: 'frontier.jpg',
+        embed: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/4fACmMByfqT7o9CakRa5FX?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+      },
+      {
+        name: 'In Absurdity',
+        year: '2022',
+        img: 'in-absurdity.jpg',
+        embed: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/1Sb3qsakbiwpCTMOYjQyqv?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+      },
+      {
+        name: 'Dr. Floydbeck',
+        year: '2020',
+        img: 'dr-floydbeck.jpg',
+        embed: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/5Q3DalaEbKtdgUnMvHjc09?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+      }
+    ]
+  },
+  'crouchandfoster.com': {
+    imgPath: '/img/crouch-and-foster',
+    pageTitle: 'Crouch and Foster - Austin, Texas based singer-songwriter duo.',
+    bandName: 'Crouch and Foster',
+    bandBio: "Crouch and Foster is an Austin, TX based singer songwriter duo that wrote and recorded from 2005 to 2011.  The duo, Austin Crouch and Ezra Foster, released a collection of rough draft and demo recordings called The Demo Tapes in 2008 and a single EP called Smellin' The Sky in 2011 before turning their attention to individual endeavors.  Despite the hiatus from Crouch and Foster, the two are still quite close and have even collaborated on Ezra Foster songs, including as recently as 2020.",
+    links: {
+      apple:'https://music.apple.com/dk/artist/crouch-foster/1541214776',
+      spotify: 'https://open.spotify.com/artist/5rNjqJPb4dZQ07XAdnyvNW',
+      amazon: 'https://www.amazon.com/music/player/artists/B08NXVHYNW/crouch-foster',
+      pandora: 'https://www.pandora.com/artist/crouch-and-foster/ARlcJzzj5Pxg4j6',
+    },
+    albums: [
+      {
+        name: "Smellin' the Sky",
+        year: '2011',
+        img: 'smellin-the-sky.jpg',
+        embed: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/6X44fslZsCA2u8V2oamAOU?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+      },
+      {
+        name: 'The Demo Tapes',
+        year: '2008',
+        img: 'demo-tapes.jpg',
+        embed: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/4V0E3a1Y3s6PqUSmnKiJOv?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+      }
     ]
   }
 }
